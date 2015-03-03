22dd2eb2a77fa90eea25d40fc10935fd55cb0170
Javi Ribera <javiribera@gmail.com>
2015-03-02 23:35:00 -0500

    code cleanup

--- main.py
+++ main.py
@@ -22,7 +22,45 @@ import cv2 as cv
 import utils
 
 
-from ipdb import set_trace as debug
+def main():
+    # parse the command line arguments to attributes of 'args'
+    parser = argparse.ArgumentParser(description='DCT compressor. '
+                                                 'Reconstructs an image using only the first K coefficients '
+                                                 'of its 8x8 DCT.')
+    parser.add_argument('--input', dest='image_path', required=True, type=str,
+                        help='Path to the image to be analyzed.')
+    parser.add_argument('--coeffs', dest='num_coeffs', required=True,
+                        type=functools.partial(utils.power_of_two, max=64),
+                        help='Number of coefficients that will be used to reconstruct the original image. 64 max.')
+    parser.add_argument('--blocksize', dest='blocksize', type=int, default=8,
+                        help='Length of the sides of the blocks the input image will be cut into.')
+    args = parser.parse_args()
+
+    # read image
+    img = cv.imread(args.image_path, cv.CV_LOAD_IMAGE_COLOR)
+    img = np.float64(img) / 255
+
+    # get YCC components
+    img_ycc = cv.cvtColor(img, code=cv.cv.CV_BGR2YCrCb, dstCn=3)
+
+    # compress and decompress every channel separately
+    rec_img = np.empty_like(img)
+    for channel_num in xrange(3):
+        mono_image = approximate_mono_image(img_ycc[:, :, channel_num],
+                                            num_coeffs=args.num_coeffs,
+                                            blocksize=args.blocksize)
+        rec_img[:, :, channel_num] = mono_image
+
+    # round to the nearest integer [0,255] value
+    rec_img = np.uint8(np.round(rec_img * 255))
+
+    # convert back to RGB
+    rec_img_rgb = cv.cvtColor(rec_img, code=cv.cv.CV_YCrCb2BGR, dstCn=3)
+
+    # visualize
+    cv.imshow('', rec_img_rgb)
+    while True:
+        cv.waitKey(33)
 
 
 def approximate_mono_image(img, num_coeffs, blocksize=8):
@@ -75,45 +113,8 @@ def approximate_mono_image(img, num_coeffs, blocksize=8):
                 rec_img.extend(block[row_block_num])
     rec_img = np.array(rec_img).reshape(height, width)
 
-    # round to the nearest integer [0,255] value
-    rec_img = np.uint8(np.round(rec_img * 255))
-
     return rec_img
 
 
 if __name__ == '__main__':
-    # parse the command line arguments to attributes of 'args'
-    parser = argparse.ArgumentParser(description='DCT compressor. '
-                                                 'Reconstructs an image using only the first K coefficients '
-                                                 'of its 8x8 DCT.')
-    parser.add_argument('--input', dest='image_path', required=True, type=str,
-                        help='Path to the image to be analyzed.')
-    parser.add_argument('--coeffs', dest='num_coeffs', required=True,
-                        type=functools.partial(utils.power_of_two, max=64),
-                        help='Number of coefficients that will be used to reconstruct the original image. 64 max.')
-    parser.add_argument('--blocksize', dest='blocksize', type=int, default=8,
-                        help='Length of the sides of the blocks the input image will be cut into.')
-    args = parser.parse_args()
-
-    # read image
-    img = cv.imread(args.image_path, cv.CV_LOAD_IMAGE_COLOR)
-    img = np.float32(img) / 255
-
-    # get YCC components
-    img_ycc = cv.cvtColor(img, code=cv.cv.CV_BGR2YCrCb, dstCn=3)
-
-    # compress and decompress every channel separately
-    rec_img = np.empty_like(img)
-    for channel_num in xrange(3):
-        mono_image = approximate_mono_image(img_ycc[:, :, channel_num],
-                                            num_coeffs=args.num_coeffs,
-                                            blocksize=args.blocksize)
-        rec_img[:, :, channel_num] = mono_image
-
-    # convert back to RGB
-    rec_img_rgb = cv.cvtColor(np.uint8(rec_img), code=cv.cv.CV_YCrCb2BGR, dstCn=3)
-
-    # visualize
-    cv.imshow('', rec_img_rgb)
-    while True:
-        cv.waitKey(33)
+    main()
